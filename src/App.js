b676170f24204493e404b575ffcdf3b4e27adc15
Victor Yohanna <victoryohanna@gmail.com>
2023-03-02 20:23:59 +0100

    modified app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import Header from "./components/Header";
 import OptionList from "./components/Options";
 import BootcampSection from "./components/Bootcamp";
+import OurPrograms from "./components/Programs";
+import Transition from "./components/Transition";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Header/>
       <OptionList/>
       <BootcampSection/>
+      <OurPrograms/>
+      <Transition/>
      </div>
     </div>
   );
