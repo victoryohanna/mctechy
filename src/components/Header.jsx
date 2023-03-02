a6fe93ab68cea854cd28a46b5eb91161f5e7b947
Victor Yohanna <victoryohanna@gmail.com>
2023-03-02 11:40:50 +0100

    add carousel to header compnent

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
+import "../styles/header.css";
+import image1 from "../assets/header1.jpg";
+import image2 from "../assets/header2.jpg";
 
-import '../styles/header.css';
-
-const Header = ()=>{
-    return(
-        <div className='container'>
-            <h1>Header</h1>
+const Header = () => {
+  return (
+    <div className="container">
+      <div className="carousel slide carousel-fade" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={image1} className="img-carousel " alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={image2} className="img-carousel" alt="..." />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
