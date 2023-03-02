cbc6133fb4e28ef6539fb74d78c80699a7b9bbaa
Victor Yohanna <victoryohanna@gmail.com>
2023-03-02 10:22:00 +0100

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+import '../styles/header.css';
+
+const Header = ()=>{
+    return(
+        <div className='container'>
+            <h1>Header</h1>
+        </div>
+    )
+}
+
+export default Header;
