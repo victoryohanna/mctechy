4a92aa7f710af5b2c771c3ce6df4829fd3d550ab
Victor Yohanna <victoryohanna@gmail.com>
2023-03-02 20:24:53 +0100

    modified bootcamp.jsx

--- src/components/Bootcamp.jsx
+++ src/components/Bootcamp.jsx
@@ -10,41 +10,40 @@ const BootcampSection = () => {
         <h1>Get Job-Ready in no time</h1>
       </div>
       <div className="container">
-      
-      <div className="seperator"></div>
-      <div className="main-section">
-        <div className="items-list">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                <h2>Evenings Only</h2>
-                <span>Got a day job? We have program for you</span>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-link active" aria-current="page">
-                <h2>Weekends Only</h2>
-                <span>Busy Week? We offer Weekend Only classes</span>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                <h2>Become Certified</h2>
-                <span>Get jon ready with the right certifications</span>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                <h2>Onsite Classes</h2>
-                <span>Get one on one interactions with tutors</span>
-              </Link>
-            </li>
-          </ul>
+        <div className="seperator"></div>
+        <div className="main-section">
+          <div className="items-list">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  <h2>Evenings Only</h2>
+                  <span>Got a day job? We have program for you</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/" className="nav-link active" aria-current="page">
+                  <h2>Weekends Only</h2>
+                  <span>Busy Week? We offer Weekend Only classes</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  <h2>Become Certified</h2>
+                  <span>Get jon ready with the right certifications</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  <h2>Onsite Classes</h2>
+                  <span>Get one on one interactions with tutors</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="image-section">
+            <img className="imag" src={image3} alt="" />
+          </div>
         </div>
-        <div className="image-section">
-          <img className="imag" src={image3} alt="" />
-        </div>
-      </div>
       </div>
     </div>
   );
