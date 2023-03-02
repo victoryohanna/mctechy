2e4cd981c3b279052ab6012bc7622537a8fa468d
Victor Yohanna <victoryohanna@gmail.com>
2023-03-02 19:06:41 +0100

    modified bootcamp component

--- src/components/Bootcamp.jsx
+++ src/components/Bootcamp.jsx
@@ -1,40 +1,42 @@
 import { Link } from "react-router-dom";
 import "../styles/bootcamp.css";
-
 import image3 from "../assets/bootcamp1.jpg";
 
 const BootcampSection = () => {
   return (
-    <div className="container container-bootcamp">
-      <div className="header-text">
-        <p>Unlock Your Potential with Our Tech Bootcamp</p>
+    <div className=" container-bootcamp">
+      <div className="container header-text">
+        <span>unlock your potential with our tech bootcamp</span>
         <h1>Get Job-Ready in no time</h1>
       </div>
+      <div className="container">
+      
+      <div className="seperator"></div>
       <div className="main-section">
         <div className="items-list">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <Link to="/" className="nav-link">
                 <h2>Evenings Only</h2>
-                <span>Got a day job? we have program for you</span>
+                <span>Got a day job? We have program for you</span>
               </Link>
             </li>
             <li className="nav-item">
               <Link to="/" className="nav-link active" aria-current="page">
                 <h2>Weekends Only</h2>
-                <span>Busy Week? We offer weekend classes</span>
+                <span>Busy Week? We offer Weekend Only classes</span>
               </Link>
             </li>
             <li className="nav-item">
               <Link to="/" className="nav-link">
                 <h2>Become Certified</h2>
-                <span>Got ready with the right certificate</span>
+                <span>Get jon ready with the right certifications</span>
               </Link>
             </li>
             <li className="nav-item">
               <Link to="/" className="nav-link">
                 <h2>Onsite Classes</h2>
-                <span>Get one on one interaction with tutors</span>
+                <span>Get one on one interactions with tutors</span>
               </Link>
             </li>
           </ul>
@@ -42,6 +44,7 @@ const BootcampSection = () => {
         <div className="image-section">
           <img className="imag" src={image3} alt="" />
         </div>
+      </div>
       </div>
     </div>
   );
