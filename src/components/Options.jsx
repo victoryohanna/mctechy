c31768a058735c08dbc188c7b1be79a9d9770ab2
Victor Yohanna <victoryohanna@gmail.com>
2023-03-02 18:24:53 +0100

    modified optionslist component

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -3,15 +3,14 @@ import "../styles/optionslist.css";
 const OptionList = () => {
   return (
     <div className="container options-container">
-      <div className="card container">
-        <div className="card-body">
+        <div className="container">
           <form>
             <div className="row">
               <div className="col-md-3">
                 <label htmlFor="" className="form-label">
                   Program
                 </label>
-                <select class="form-select" aria-label="Default select example">
+                <select className="form-select" aria-label="Default select example">
                   <option selected>Frontend Development</option>
                   <option value="1">One</option>
                   <option value="2">Two</option>
@@ -22,8 +21,8 @@ const OptionList = () => {
                 <label htmlFor="" className="form-label">
                   Schedule
                 </label>
-                <select class="form-select" aria-label="Default select example">
-                  <option selected>Full Time </option>
+                <select className="form-select" aria-label="Default select example">
+                  <option selected>Full Time (3 Months)</option>
                   <option value="1">One</option>
                   <option value="2">Two</option>
                   <option value="3">Three</option>
@@ -39,14 +38,14 @@ const OptionList = () => {
                   placeholder="Your email address"
                 />
               </div>
-              <div className="col-md-3">
-                <button className="btn btn-primary">I'm interested</button>
+              <div className="col-md-3 btn-section">
+                <button type="button" className="">I'm interested</button>
               </div>
             </div>
           </form>
         </div>
       </div>
-    </div>
+    
   );
 };
 
