d2fe4a9f896b4ae36cda00fa705ad1dea3419edf
Victor Yohanna <victoryohanna@gmail.com>
2023-03-03 11:09:14 +0100

    modified components

---
+++ src/components/elements/Button.jsx
@@ -0,0 +1,10 @@
+import '../../styles/elements/button.css'
+const Button = ()=>{
+    return(
+        <div className="btn-section">
+            <button type="button">Get Started</button>
+        </div>
+    )
+}
+
+export default Button;
