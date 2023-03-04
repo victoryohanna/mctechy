0353d21b8fb56330743d3dadfe9161d428cafbaa
Victor Yohanna <victoryohanna@gmail.com>
2023-03-04 17:20:58 +0100

    add files

---
+++ src/components/files/programlist.js
@@ -0,0 +1,70 @@
+
+export const frontendDev = [
+    {
+        time: "8:30",
+        item2: "Html",
+        item3: "CSS",
+        item4: "80%",
+    },
+    {
+        item1: "9:20",
+        item2: "Sass",
+        item3: "Xml",
+        item4: "60&",
+    },
+    {
+        item1: "10:00",
+        item2: "Chrome",
+        item3: "Firefox",
+        item4: "87%",
+    },
+    {
+        item1: "12:30",
+        item2: "Bootstrap",
+        item3: "Tailwin",
+        item4: "67%",
+    },
+    {
+        item1: "10:30",
+        item2: "React Native",
+        item3: "Kotlin",
+        item4: "72%",
+    },
+]
+
+export const backendDev = [
+    {
+        item1: "",
+        item2: "",
+        item3: "",
+        item4: "",
+    }
+]
+
+export const productDesign = [
+    {
+        item1: "",
+        item2: "",
+        item3: "",
+        item4: "",
+    }
+]
+
+export const projectMgt = [
+    {
+        item1: "",
+        item2: "",
+        item3: "",
+        item4: "",
+    }
+]
+
+export const businessAnalytics = [
+    {
+        item1: "",
+        item2: "",
+        item3: "",
+        item4: "",
+    }
+]
+
--- src/components/images.js
+++
@@ -1,5 +0,0 @@
-
-export { default as bootcamp1} from '../assets/bootcamp1.jpg';
-export { default as bootcamp2} from '../assets/bootcamp2.jpg';
-export { default as bootcamp3} from '../assets/bootcamp3.jpg';
-export { default as bootcamp4} from '../assets/bootcamp4.jpg';
