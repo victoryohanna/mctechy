a81970ce6ade847d7ae638d21d4dabc3c288f0a2
Victor Yohanna <victoryohanna@gmail.com>
2023-03-05 23:45:04 +0100

    creates files dir

--- src/components/files/programlist.js
+++ src/components/files/programlist.js
@@ -1,7 +1,7 @@
 
 export const frontendDev = [
     {
-        time: "8:30",
+        item1: "8:30",
         item2: "Html",
         item3: "CSS",
         item4: "80%",
@@ -10,7 +10,7 @@ export const frontendDev = [
         item1: "9:20",
         item2: "Sass",
         item3: "Xml",
-        item4: "60&",
+        item4: "60%",
     },
     {
         item1: "10:00",
@@ -22,49 +22,134 @@ export const frontendDev = [
         item1: "12:30",
         item2: "Bootstrap",
         item3: "Tailwin",
-        item4: "67%",
+        item4: "65%",
     },
     {
         item1: "10:30",
         item2: "React Native",
         item3: "Kotlin",
-        item4: "72%",
+        item4: "45%",
     },
 ]
 
 export const backendDev = [
     {
-        item1: "",
-        item2: "",
-        item3: "",
-        item4: "",
-    }
+        item1: "10:25",
+        item2: "Node.Js",
+        item3: "Python",
+        item4: "70%",
+    },
+    {
+        item1: "9:30",
+        item2: "Java",
+        item3: "Docker",
+        item4: "70%",
+    },
+    {
+        item1: "11:30",
+        item2: "Express",
+        item3: "Node.Js",
+        item4: "78%",
+    },
+    {
+        item1: "12:30",
+        item2: "MongoDB",
+        item3: "MySql",
+        item4: "25%",
+    },
 ]
 
 export const productDesign = [
     {
-        item1: "",
-        item2: "",
-        item3: "",
-        item4: "",
-    }
+        item1: "9:20",
+        item2: "Intro. to Design",
+        item3: "Methodology",
+        item4: "30%",
+    },
+    {
+        item1: "10:20",
+        item2: "Intro. to Design",
+        item3: "Methodology",
+        item4: "55%",
+    },
+    {
+        item1: "11:20",
+        item2: "Intro. to Design",
+        item3: "Methodology",
+        item4: "40%",
+    },
+    {
+        item1: "12:30",
+        item2: "Intro. to Design",
+        item3: "Methodology",
+        item4: "60%",
+    },
 ]
 
 export const projectMgt = [
     {
-        item1: "",
-        item2: "",
-        item3: "",
-        item4: "",
+        item1: "8:30",
+        item2: "Project Management",
+        item3: "Project Management",
+        item4: "50%",
+    },
+
+    {
+        item1: "10:30",
+        item2: "Project Management",
+        item3: "Project Management",
+        item4: "45%",
+    },
+
+    {
+        item1: "11:30",
+        item2: "Project Management",
+        item3: "Project Management",
+        item4: "78%",
+    },
+
+    {
+        item1: "12:30",
+        item2: "Project Management",
+        item3: "Project Management",
+        item4: "69%",
+    },
+
+    {
+        item1: "1:30",
+        item2: "Project Management",
+        item3: "Project Management",
+        item4: "60%",
+    },
+
+    {
+        item1: "2:30",
+        item2: "Project Management",
+        item3: "Project Management",
+        item4: "80%",
     }
 ]
 
 export const businessAnalytics = [
     {
-        item1: "",
-        item2: "",
-        item3: "",
-        item4: "",
-    }
+        item1: "7:30",
+        item2: "Ms Excel",
+        item3: "Power BI",
+        item4: "45%",
+    },
+
+    {
+        item1: "9:30",
+        item2: "Tableau",
+        item3: "Probability Theory",
+        item4: "35%",
+    },
+    {
+        item1: "12:00",
+        item2: "Deep Learning",
+        item3: "Power BI",
+        item4: "20%",
+    },
+
 ]
 
