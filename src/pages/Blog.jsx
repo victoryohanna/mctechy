c6bce1826f86108912f5a925ffc80047c186dfb5
Victor Yohanna <victoryohanna@gmail.com>
2023-03-06 15:26:44 +0100

    creates blog and contact page

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,12 @@
+
+const Blogs = ()=>{
+    return(
+        <div className="container">
+            <div className="d-flex justify-content-center">
+                <h1>Our Blog Post</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Blogs
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+
+const ContactUs = ()=>{
+    return(
+        <div className="container">
+            <div className="d-flex justify-content-center">
+                <h1>Contact Us</h1>
+            </div>
+        </div>
+    )
+}
+
+export default ContactUs;
