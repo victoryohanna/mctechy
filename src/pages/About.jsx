1fc13bf503ac4ae1039140d89c19e995ce7a6db9
Victor Yohanna <victoryohanna@gmail.com>
2023-03-06 14:52:38 +0100

    creates AboutUs page

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+const AboutUs =()=>{
+    return(
+        <div className="container">
+            <div className="d-flex justify-content-center">
+            <h1>About McTechy</h1>
+            </div>
+        </div>
+    )
+}
+
+export default AboutUs;
